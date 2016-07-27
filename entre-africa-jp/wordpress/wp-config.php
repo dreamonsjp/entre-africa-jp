ba67c629b6c6d40ade177bf4198c9a163be33b26
Junichi Kawai <kawai@dreamons.jp>
2016-07-27 21:29:26 +0900

    merged

--- entre-africa-jp/wordpress/wp-config.php
+++ entre-africa-jp/wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'entreafrica_dev');
+define('DB_NAME', 'entreafrica');
 
 /** MySQL database username */
-define('DB_USER', 'entreafrica_dev');
+define('DB_USER', 'entreafrica');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'LexPSmzbteCw4yCT');
+define('DB_PASSWORD', 'GnhfVpB2jZFUFCda');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
